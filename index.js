3f478725fb5d9a8272859977a2bbe4f39d1524de
Anumitha Apollo <anumithaapollo12@gmail.com>
2021-07-28 23:51:19 +0530

    change in JS

--- index.js
+++ index.js
@@ -5,14 +5,10 @@ let countEl = document.getElementById("count-el");
 let count = 0;
 
 function increment() {
-  count = +1;
+  count = count + 1;
   countEl.innerText = count;
 }
 
 console.log(count);
 
 let saveButton = document.getElementById("save-btn");
-
-function save() {
-  console.log(count);
-}
